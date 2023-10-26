4d013b0a621b2670b8c038f846d9a85c74d4400f
CopyDemon <shengdevelop@gmail.com>
2023-10-26 12:57:56 -0700

    update diagnostic url point to fastapi endpoint

--- IDAES-UI/src/components/flowsheet_main_component/flowsheet_diagnostics_component/flowsheet_diagnostics_component.tsx
+++ IDAES-UI/src/components/flowsheet_main_component/flowsheet_diagnostics_component/flowsheet_diagnostics_component.tsx
@@ -8,7 +8,8 @@ import "./flowsheet_diagnostics.css";
 export default function FlowsheetDiagnostics(){
     useEffect(()=>{
         // let diagnosticDataURL = "/public/data/diagnostic_example.json";
-        let diagnosticDataURL = "localhost:6600/diagnostics";
+        //this endpoint point to fastapi
+        let diagnosticDataURL = "http://localhost:6600/diagnostics";
         let diagnosticMain = new Diagnostic_main(diagnosticDataURL);
     },[])
     return (
